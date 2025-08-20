da3cc7e5ae59995bb79ee2efbb984f361c52b00a
RakhimovAns <ansarrakhimov91@gmail.com>
2025-08-20 22:47:54 +0300

    L1.1

---
+++ 1/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	Name string
+	Age  int
+}
+
+func (h Human) SayHello() {
+	fmt.Printf("Привет, меня зовут %s, мне %d лет\n", h.Name, h.Age)
+}
+
+type Action struct {
+	Human
+}
+
+func (a Action) Run() {
+	fmt.Printf("%s бежит!\n", a.Name)
+}
+
+func main() {
+	a := Action{
+		Human: Human{Name: "Ансар", Age: 17},
+	}
+
+	a.SayHello()
+
+	a.Run()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RahkimovAns/L1
+
+go 1.24.5
